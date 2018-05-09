caa54ebfb868ecfad8790d8d3cd4f2875daa47e7
Alex Skrypnyk <alex.designworks@gmail.com>
2018-05-10 01:15:56 +1000

    [SDPA-336] Added DB-lead deployments.

---
+++ scripts/drupal/import-db.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+##
+# Import prod db.
+#
+
+# Clear cache if db found.
+[[ "$(drush core-status bootstrap --pipe)" != "" ]] && drush cr
+
+drush sql-drop -y
+drush sqlc < /tmp/.data/db.sql
+drush php-eval "module_load_install('vicgovau_core'); vicgovau_core_create_demo_users();"
+drush sqlsan --sanitize-password=DpcFakePass --sanitize-email=user+%uid@localhost -y
