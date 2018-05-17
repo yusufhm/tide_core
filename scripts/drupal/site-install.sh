f60b9e15ed1163f2a392af26386d37ad35982b2f
Alex Skrypnyk <alex.designworks@gmail.com>
2018-05-17 17:13:51 +1000

    Fixed API deployment config.

--- scripts/drupal/site-install.sh
+++ scripts/drupal/site-install.sh
@@ -5,12 +5,9 @@
 PROFILE=tide
 ADMIN_NAME=admin
 ADMIN_PASSWORD=admin
-SITE_CORE=vicgovau_core
 DOCROOT=/app/docroot
 
 drush -r $DOCROOT si $PROFILE -y --account-name=$ADMIN_NAME --account-pass=$ADMIN_PASSWORD install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL
-drush -r $DOCROOT php-eval "module_load_install('vicgovau_core'); vicgovau_core_enable_modules();"
+drush -r $DOCROOT php-eval "module_load_install('vicgovau_core'); vicgovau_core_enable_modules(TRUE);"
 drush -r $DOCROOT ublk 1
-drush -r $DOCROOT cr -y
-drush -r $DOCROOT en $SITE_CORE -y
 drush -r $DOCROOT cr -y
