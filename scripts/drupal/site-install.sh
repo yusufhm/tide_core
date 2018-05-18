50dd503539d08106a6256419b551a840b3faa5c7
Alex Skrypnyk <alex.designworks@gmail.com>
2018-05-18 11:31:54 +1000

    Moved shields enabling.

--- scripts/drupal/site-install.sh
+++ scripts/drupal/site-install.sh
@@ -8,6 +8,6 @@ ADMIN_PASSWORD=admin
 DOCROOT=/app/docroot
 
 drush -r $DOCROOT si $PROFILE -y --account-name=$ADMIN_NAME --account-pass=$ADMIN_PASSWORD install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL
-drush -r $DOCROOT php-eval "module_load_install('vicgovau_core'); vicgovau_core_enable_modules(TRUE);"
 drush -r $DOCROOT ublk 1
 drush -r $DOCROOT cr -y
+drush -r $DOCROOT php-eval "module_load_install('vicgovau_core'); vicgovau_core_enable_modules(TRUE);"
