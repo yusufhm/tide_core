b6302a3d30261daaa10eec61ddb700b3e19b8924
Sonny Kieu <sonnykt@users.noreply.github.com>
2018-06-05 17:39:46 +1000

    [SDPA-352] Returned absolute URL for media. (#127)

--- docroot/sites/default/envs/ci/settings.php
+++ docroot/sites/default/envs/ci/settings.php
@@ -10,3 +10,5 @@ $settings['environment'] = 'ci';
 // Disable shield config.
 $config['shield.settings']['credentials']['shield']['user'] = '';
 $config['shield.settings']['credentials']['shield']['pass'] = '';
+
+$config['config_split.config_split.ci']['status'] = TRUE;
