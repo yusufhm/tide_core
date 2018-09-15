c717779fc7f2b7f468f39e9170419f5e5c3225c2
enzo - Eduardo Garcia <enzo@enzolutions.com>
2018-09-15 17:48:42 +1000

    [SDPA-884] Event Category Taxonomy (#252)
    * Add event vocabulary config, update composer to tide with tide_event as dependency
    
    * Update composer files to add tide_event dependency
    
    * Update composer.lock
    
    * Update composer.lock
    
    * Add default content module and export event default terms
    
    * update default content namespace definition
    
    * update default content namespace definition
    
    * Enable module tide_event and default content
    
    * Update composer.lock
    
    * Force import configuration
    
    * Force import configuration
    
    * Add missing term and add demo functions
    
    * Update tide_event repo source
    
    * Fix code indent
    
    * Revert "Fix code indent"
    
    This reverts commit 173b529e047697c709f418f541597010c071e490.
    
    * Fix code indent
    
    * Fix code indent
    
    * Remove taxonomy event contig and enable tide_event via code
    
    * Fix code indent
    
    * Include tide_event in core extension configuration
    
    * Restore event taxonomy config
    
    * Restore vicgovcore module file
    
    * Disable tide_event install
    
    * remove taxonomy configuration , enable default_content
    
    * Add hal module to be enabled
    
    * remove default content and hal, enable default content via hook update
    
    * remove default content and hal, enable default content via hook update
    
    * remove default content and hal, enable default content via hook update
    
    * remove default content and hal, enable default content via hook update
    
    * add event taxonoy config and remove tide event install
    
    * Enable tide event module
    
    * Remove hook update, add helper to import default content
    
    * Update deploy to load module functions
    
    * Update deploy to load module functions
    
    * Use hook update to enable content and remove from deploy
    
    * Use hook update to enable content and remove from deploy
    
    * Create terms programmatically
    
    * Create terms using default content
    
    * Add default content after config import in deploy
    
    * Fix code indent
    
    * remove hal
    
    * Update composer lock, remove tide branch
    
    * Upgrade to tide 0.0.2

--- scripts/drupal/deploy.sh
+++ scripts/drupal/deploy.sh
@@ -13,4 +13,6 @@ drush cim -y
 drush cr -y
 echo "Enabling vicgovau modules"
 drush php-eval "module_load_install('vicgovau_core'); vicgovau_core_enable_modules();"
+echo "Importing vicgovau default content"
+drush php-eval "module_load_install('vicgovau_core'); vicgovau_core_default_content_modules();"
 drush cr -y
