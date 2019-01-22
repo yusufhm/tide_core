fe089c141123bb9704af1f530b868b15fcd7e557
markxtji <markxtji@gmail.com>
2019-01-22 14:21:47 +1100

    updated the predeployment backup script (#431)
    * updated the predeployment backup script

--- scripts/drupal/backup.sh
+++ scripts/drupal/backup.sh
@@ -3,5 +3,18 @@
 # Pre deployment db backup.
 #
 
-mkdir -p /app/docroot/sites/default/files/private
-drush sql-dump --gzip --result-file=/app/docroot/sites/default/files/private/pre_deploy_backup.sql
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+BACKUP_DIR="/app/docroot/sites/default/files/private/"
+BACKUP_FILE="pre_deploy_backup"
+RETENTION_DAYS=1
+
+#backup database
+mkdir -p ${BACKUP_DIR}
+drush sql-dump --gzip --result-file=${BACKUP_DIR}${BACKUP_FILE}.sql
+
+
+#add time stamp to the backup files
+mv ${BACKUP_DIR}${BACKUP_FILE}.sql.gz ${BACKUP_DIR}${BACKUP_FILE}.${DATE_WITH_TIME}.sql.gz
+
+# remove files older than 1 day
+find ${BACKUP_DIR} -type f -name "${BACKUP_FILE}*.sql.gz" -mtime ${RETENTION_DAYS} -exec rm {} \;
