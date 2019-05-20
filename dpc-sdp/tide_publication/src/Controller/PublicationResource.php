ecadebb127b84c34dd8e750049921d5e1bad35bb
Sonny Kieu <sonny@salsadigital.com.au>
2019-05-20 19:12:36 +1000

    [SDPA-128] Added custom route to retrieve publication hierarchy.
    On-behalf-of: @salsadigitalauorg <sonny@salsadigital.com.au>

---
+++ dpc-sdp/tide_publication/src/Controller/PublicationResource.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace Drupal\tide_publication\Controller;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\entity_hierarchy\Storage\EntityTreeNodeMapperInterface;
+use Drupal\entity_hierarchy\Storage\NestedSetNodeKeyFactory;
+use Drupal\entity_hierarchy\Storage\NestedSetStorage;
+use Drupal\entity_hierarchy\Storage\NestedSetStorageFactory;
+use Drupal\jsonapi\Controller\EntityResource;
+use Drupal\jsonapi\Exception\EntityAccessDeniedHttpException;
+use Drupal\jsonapi\JsonApiResource\Link;
+use Drupal\jsonapi\JsonApiResource\LinkCollection;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class PublicationResource.
+ *
+ * @package Drupal\tide_publication\Controller
+ */
+class PublicationResource extends EntityResource {
+  use ContainerAwareTrait;
+
+  /**
+   * The NestedSetStorageFactory service.
+   *
+   * @var \Drupal\entity_hierarchy\Storage\NestedSetStorageFactory
+   */
+  protected $nestedSetStorageFactory;
+
+  /**
+   * The NestedSetNodeKeyFactory service.
+   *
+   * @var \Drupal\entity_hierarchy\Storage\NestedSetNodeKeyFactory
+   */
+  protected $nestedSetNodeKeyFactory;
+
+  /**
+   * The EntityTreeNodeMapperInterface service.
+   *
+   * @var \Drupal\entity_hierarchy\Storage\EntityTreeNodeMapperInterface
+   */
+  protected $entityTreeNodeMapper;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The cache bin.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cacheData;
+
+  /**
+   * Injects dependencies.
+   *
+   * @param \Drupal\entity_hierarchy\Storage\NestedSetStorageFactory $nested_set_storage_factory
+   *   NestedSetStorageFactory service.
+   * @param \Drupal\entity_hierarchy\Storage\NestedSetNodeKeyFactory $nested_set_nodekey_factory
+   *   NestedSetNodeKeyFactory service.
+   * @param \Drupal\entity_hierarchy\Storage\EntityTreeNodeMapperInterface $entity_tree_node_mapper
+   *   EntityTreeNodeMapperInterface service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   Module handler service.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_data
+   *   Data cache bin.
+   */
+  public function setDependencies(NestedSetStorageFactory $nested_set_storage_factory, NestedSetNodeKeyFactory $nested_set_nodekey_factory, EntityTreeNodeMapperInterface $entity_tree_node_mapper, ModuleHandlerInterface $module_handler, CacheBackendInterface $cache_data) {
+    $this->nestedSetStorageFactory = $nested_set_storage_factory;
+    $this->nestedSetNodeKeyFactory = $nested_set_nodekey_factory;
+    $this->entityTreeNodeMapper = $entity_tree_node_mapper;
+    $this->moduleHandler = $module_handler;
+    $this->cacheData = $cache_data;
+  }
+
+  /**
+   * Get the hierarchy of a publication.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The loaded entity.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object.
+   *
+   * @return \Drupal\jsonapi\ResourceResponse|\Symfony\Component\HttpFoundation\JsonResponse
+   *   The response.
+   *
+   * @throws \Drupal\jsonapi\Exception\EntityAccessDeniedHttpException|\Exception
+   *   Thrown when access to the entity is not allowed.
+   */
+  public function getHierarchy(EntityInterface $entity, Request $request) {
+    if (!($entity instanceof ContentEntityInterface)) {
+      throw new \Exception('The requested entity does not support hierarchy.');
+    }
+
+    $resource_object = $this->entityAccessChecker->getAccessCheckedResourceObject($entity);
+    if ($resource_object instanceof EntityAccessDeniedHttpException) {
+      throw $resource_object;
+    }
+
+    if ($entity->bundle() != 'publication') {
+      $response = $this->buildWrappedResponse($resource_object, $request, $this->getIncludes($request, $resource_object));
+      return $response;
+    }
+
+    if (!$this->nestedSetStorageFactory || !$this->nestedSetNodeKeyFactory || !$this->entityTreeNodeMapper) {
+      throw new \Exception('The method setEntityHierarchyServices() must be called when instantiating a PublicationResource object.');
+    }
+
+    $resource_type = $this->resourceTypeRepository->get($entity->getEntityTypeId(), $entity->bundle());
+    $entity_link = $this->linkManager->getEntityLink($entity->uuid(), $resource_type, [], 'individual');
+
+    $response_data = [
+      'type' => $resource_type->getTypeName(),
+      'id' => $entity->uuid(),
+      'links' => [
+        'self' => [
+          'href' => $entity_link,
+        ],
+      ],
+    ];
+
+    $cache = new CacheableMetadata();
+    $cache->addCacheableDependency($entity);
+
+    $site = NULL;
+    if ($this->moduleHandler->moduleExists('tide_site')) {
+      /** @var \Drupal\tide_site\TideSiteHelper $site_helper */
+      $site_helper = $this->container->get('tide_site.helper');
+      $site_id = $request->get('site');
+      if ($site_helper->isValidSite($site_id)) {
+        $site = $site_helper->getSiteById($site_id);
+      }
+    }
+
+    $publication_field_name = 'field_publication';
+    /** @var \PNX\NestedSet\NestedSetInterface $storage */
+    $storage = $this->nestedSetStorageFactory->get($publication_field_name, $entity->getEntityTypeId());
+    $flatten_hierarchy = [];
+    $hierarchy = $this->buildHierarchy($entity, $storage, $cache, NULL, $flatten_hierarchy, $site);
+    $response_data['meta']['hierarchy'] = $hierarchy;
+
+    // Cache the flatten hierarchy.
+    if (!empty($flatten_hierarchy)) {
+      $cid = 'tide_publication:hierarchy:' . $entity->uuid() . ':flatten';
+      $this->cacheData->set($cid, $flatten_hierarchy, $cache->getCacheMaxAge(), $cache->getCacheTags());
+    }
+
+    $self_link = new Link(new CacheableMetadata(), $this->linkManager->getRequestLink($request), ['self']);
+    $links = (new LinkCollection([]))->withLink('self', $self_link);
+    $serialized_links = $this->serializer->normalize($links, 'api_json');
+
+    return new JsonResponse([
+      'data' => $response_data,
+      'links' => $serialized_links->getNormalization(),
+    ], 200);
+  }
+
+  /**
+   * Build the hierarchy.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity.
+   * @param \Drupal\entity_hierarchy\Storage\NestedSetStorage $storage
+   *   The storage.
+   * @param \Drupal\Core\Cache\CacheableMetadata $cache
+   *   The cache object.
+   * @param int|null $weight
+   *   The weight of the current entity.
+   * @param array $flatten_hierarchy
+   *   The flatten hierarchy to be built.
+   * @param \Drupal\taxonomy\TermInterface|null $site
+   *   The current site term.
+   *
+   * @return array
+   *   The hierarchy.
+   */
+  protected function buildHierarchy(ContentEntityInterface $entity, NestedSetStorage $storage, CacheableMetadata $cache, $weight, array &$flatten_hierarchy, $site = NULL) {
+    $resource_type = $this->resourceTypeRepository->get($entity->getEntityTypeId(), $entity->bundle());
+
+    $hierarchy = [
+      'id' => $entity->uuid(),
+      'type' => $resource_type->getTypeName(),
+      'entity_id' => $entity->id(),
+      'title' => $entity->label(),
+      'url' => $entity->toUrl('canonical')->toString(),
+      'weight' => $weight,
+    ];
+
+    $flatten_hierarchy[] = [
+      'entity_id' => $entity->id(),
+      'revision_id' => $entity->getRevisionId(),
+      'bundle' => $entity->bundle(),
+      'uuid' => $entity->uuid(),
+      'weight' => $weight,
+    ];
+
+    if ($site) {
+      /** @var \Drupal\tide_site\TideSiteHelper $site_helper */
+      $site_helper = $this->container->get('tide_site.helper');
+      if (!$site_helper->isEntityBelongToSite($entity, $site->id())) {
+        $hierarchy['url'] = $site_helper->getNodeUrlFromPrimarySite($entity);
+      }
+    }
+
+    /** @var \PNX\NestedSet\NestedSetInterface $storage */
+    /** @var \PNX\NestedSet\Node[] $children */
+    $children = $storage->findChildren($this->nestedSetNodeKeyFactory->fromEntity($entity));
+    $child_entities = $this->entityTreeNodeMapper->loadAndAccessCheckEntitysForTreeNodes('node', $children, $cache);
+    foreach ($children as $child_weight => $nested_node) {
+      if (!$child_entities->contains($nested_node)) {
+        // Doesn't exist or is access hidden.
+        continue;
+      }
+      /** @var \Drupal\Core\Entity\ContentEntityInterface $child_entity */
+      $child_entity = $child_entities->offsetGet($nested_node);
+      if (!$child_entity->isDefaultRevision()) {
+        continue;
+      }
+
+      $cache->addCacheableDependency($child_entity);
+      $hierarchy['children'][] = $this->buildHierarchy($child_entity, $storage, $cache, $child_weight, $flatten_hierarchy, $site);
+    }
+
+    return $hierarchy;
+  }
+
+}
---
+++ dpc-sdp/tide_publication/src/Routing/Routes.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\tide_publication\Routing;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\jsonapi\ParamConverter\ResourceTypeConverter;
+use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
+use Drupal\jsonapi\Routing\Routes as JsonapiRoutes;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class Routes.
+ *
+ * @package Drupal\tide_publication\Routing
+ */
+class Routes implements ContainerInjectionInterface {
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The JSON:API resource type repository.
+   *
+   * @var \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface
+   */
+  protected $resourceTypeRepository;
+
+  /**
+   * List of providers.
+   *
+   * @var string[]
+   */
+  protected $providerIds;
+
+  /**
+   * The JSON:API base path.
+   *
+   * @var string
+   */
+  protected $jsonApiBasePath;
+
+  /**
+   * Instantiates a Routes object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
+   * @param \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface $resource_type_repository
+   *   The JSON:API resource type repository.
+   * @param string[] $authentication_providers
+   *   The authentication providers, keyed by ID.
+   * @param string $jsonapi_base_path
+   *   The JSON:API base path.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler, ResourceTypeRepositoryInterface $resource_type_repository, array $authentication_providers, $jsonapi_base_path) {
+    $this->moduleHandler = $module_handler;
+    $this->resourceTypeRepository = $resource_type_repository;
+    $this->providerIds = array_keys($authentication_providers);
+    $this->jsonApiBasePath = $jsonapi_base_path;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('module_handler'),
+      $container->get('jsonapi.resource_type.repository'),
+      $container->getParameter('authentication_providers'),
+      $container->getParameter('jsonapi.base_path')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function routes() {
+    $routes = new RouteCollection();
+
+    if ($this->moduleHandler->moduleExists('jsonapi')) {
+      $resource_type = $this->resourceTypeRepository->get('node', 'publication');
+      $path = $resource_type->getPath();
+      $entity_type_id = $resource_type->getEntityTypeId();
+
+      $hierarchy_route = new Route("/{$this->jsonApiBasePath}{$path}/{entity}/hierarchy");
+      $hierarchy_route->addDefaults([RouteObjectInterface::CONTROLLER_NAME => JsonapiRoutes::CONTROLLER_SERVICE_NAME . '.publication:getHierarchy']);
+      $hierarchy_route->addDefaults([JsonapiRoutes::RESOURCE_TYPE_KEY => $resource_type->getTypeName()]);
+      $hierarchy_route->setMethods(['GET']);
+      $hierarchy_route->setRequirement('_access', 'TRUE');
+      $hierarchy_route->addRequirements(['_format' => 'api_json']);
+      $hierarchy_route->addOptions(['parameters' => ['entity' => ['type' => 'entity:' . $entity_type_id]]]);
+
+      $parameters = $hierarchy_route->getOption('parameters') ?: [];
+      $parameters[JsonapiRoutes::RESOURCE_TYPE_KEY] = ['type' => ResourceTypeConverter::PARAM_TYPE_ID];
+      $hierarchy_route->setOption('parameters', $parameters);
+
+      $routes->add(JsonapiRoutes::getRouteName($resource_type, 'individual.hierarchy'), $hierarchy_route);
+
+      // Require the JSON:API media type header on every route, except on file
+      // upload routes, where we require `application/octet-stream`.
+      $routes->addRequirements(['_content_type_format' => 'api_json']);
+      // Enable all available authentication providers.
+      $routes->addOptions(['_auth' => $this->providerIds]);
+      // Flag every route as belonging to the JSON:API module.
+      $routes->addDefaults([JsonapiRoutes::JSON_API_ROUTE_FLAG_KEY => TRUE]);
+      // All routes serve only the JSON:API media type.
+      $routes->addRequirements(['_format' => 'api_json']);
+    }
+
+    return $routes;
+  }
+
+}
