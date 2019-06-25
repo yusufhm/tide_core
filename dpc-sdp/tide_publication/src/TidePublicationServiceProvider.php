b049c1ca5356c85995b22e4a26c684678c7ae967
Sonny Kieu <sonny@salsadigital.com.au>
2019-06-25 17:06:45 +1000

    Upgraded JSON:API and JSON:API Extras.

---
+++ dpc-sdp/tide_publication/src/TidePublicationServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\tide_publication;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class TidePublicationServiceProvider.
+ *
+ * @package Drupal\tide_site
+ */
+class TidePublicationServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function register(ContainerBuilder $container) {
+    // Dynamically define the service tide_site.get_route_subscriber.
+    $modules = $container->getParameter('container.modules');
+
+    // Check for installed tide_api module.
+    if (isset($modules['tide_api'])) {
+      if ($container->hasDefinition('jsonapi.entity_resource')) {
+        $entity_resource = $container->getDefinition('jsonapi.entity_resource');
+        // Pretends to be a JSON:API Controller.
+        $container->register('jsonapi.entity_resource.publication', '\Drupal\tide_publication\Controller\PublicationResource')
+          ->setArguments($entity_resource->getArguments())
+          ->addMethodCall('setContainer', [new Reference('service_container')])
+          ->addMethodCall('setDependencies', [
+            new Reference('entity_hierarchy.nested_set_storage_factory'),
+            new Reference('entity_hierarchy.nested_set_node_factory'),
+            new Reference('entity_hierarchy.entity_tree_node_mapper'),
+            new Reference('module_handler'),
+            new Reference('cache.data'),
+          ]);
+      }
+    }
+  }
+
+}
