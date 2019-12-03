caf86b08220125b1447c1afd61f449485c15ccfe
anthony-malkoun <anthony.malkoun@dpc.vic.gov.au>
2019-12-03 17:41:38 +1100

    Update to drupal 8.7.9 (#106)
    * Update Drupal version to 8.7
    
    * Fix error with Drupal version
    
    * Update stage_file_proxy version to work with Drupal 8.7.9
    
    * comment GH_COMMIT in dev-tools.sh
    
    * reverting dev-tools and stage_file_proxy

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=08030a69a651ad270e4d5328c69a341c21855f94
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
