e091b57f5d93f7639a83eae7a70344bc288c8b72
Sonny Kieu <sonny@salsadigital.com.au>
2020-12-08 15:30:38 +1100

    Added submodule Tide OAuth. (#172)
    * Added submodule Tide OAuth.
    
    On-behalf-of: @salsadigitalauorg <sonny@salsadigital.com.au>

---
+++ modules/tide_oauth/src/Authentication/Provider/XSimpleOauthAuthenticationProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\tide_oauth\Authentication\Provider;
+
+use Drupal\simple_oauth\Authentication\Provider\SimpleOauthAuthenticationProvider;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class XSimpleOauthAuthenticationProvider.
+ *
+ * @internal
+ * @package Drupal\tide_oauth\Authentication\Provider
+ */
+class XSimpleOauthAuthenticationProvider extends SimpleOauthAuthenticationProvider {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function authenticate(Request $request) {
+    // X-OAuth2-Authorization does not comply to OAuth2 so that we need to
+    // set Authorization header as per the OAuth2 specs.
+    // However, Authorization header will trigger JWT Authentication (if exists)
+    // hence we need to clone the request instead of modifying the original.
+    $oauth2_request = clone $request;
+    $auth_header = trim($request->headers->get('Authorization', '', TRUE));
+    if ((strpos($auth_header, 'OAuth2 ') !== FALSE) || ($auth_header === 'OAuth2')) {
+      $oauth2_request->headers->add([
+        'Authorization' => str_replace('OAuth2', 'Bearer', $auth_header),
+      ]);
+    }
+    else {
+      $x_auth_header = trim($oauth2_request->headers->get('X-OAuth2-Authorization', '', TRUE));
+      if (($x_auth_header === 'Bearer') || (strpos($x_auth_header, 'Bearer ') !== FALSE)) {
+        $oauth2_request->headers->add(['Authorization' => $x_auth_header]);
+      }
+      elseif (($x_auth_header === 'OAuth2') || (strpos($x_auth_header, 'OAuth2 ') !== FALSE)) {
+        $oauth2_request->headers->add([
+          'Authorization' => str_replace('OAuth2', 'Bearer', $x_auth_header),
+        ]);
+      }
+    }
+
+    $account = parent::authenticate($oauth2_request);
+    if ($account) {
+      // Inherit uploaded files for the current request.
+      /* @link https://www.drupal.org/project/drupal/issues/2934486 */
+      $request->files->add($oauth2_request->files->all());
+      // Set consumer ID header on successful authentication, so negotiators
+      // will trigger correctly.
+      $request->headers->set('X-Consumer-ID', $account->getConsumer()->uuid());
+    }
+
+    return $account;
+  }
+
+}
---
+++ modules/tide_oauth/src/Commands/TideOauthCommands.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\tide_oauth\Commands;
+
+use Drupal\tide_oauth\EnvKeyGenerator;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Class TideOauthCommands.
+ *
+ * @package Drupal\tide_oauth\Commands
+ */
+class TideOauthCommands extends DrushCommands {
+
+  /**
+   * Env Key Generator.
+   *
+   * @var \Drupal\tide_oauth\EnvKeyGenerator
+   */
+  protected $envKeyGenerator;
+
+  /**
+   * TideOauthCommands constructor.
+   *
+   * @param \Drupal\tide_oauth\EnvKeyGenerator $env_key_generator
+   *   Env Key Generator.
+   */
+  public function __construct(EnvKeyGenerator $env_key_generator) {
+    parent::__construct();
+    $this->envKeyGenerator = $env_key_generator;
+  }
+
+  /**
+   * Generate OAuth keys from Environment variables.
+   *
+   * @usage drush tide-oauth:keygen
+   *   Generate OAuth keys from Environment variables.
+   *
+   * @command tide-oauth:keygen
+   * @validate-module-enabled tide_oauth
+   * @aliases tokgn,tide-oauth-keygen
+   */
+  public function generateKeys() {
+    if ($this->envKeyGenerator->generateEnvKeys()) {
+      // Update Simple OAuth settings.
+      $this->envKeyGenerator->setSimpleOauthKeySettings();
+      $this->io()->success('OAuth keys have been created.');
+    }
+    else {
+      $this->io()->error('Could not generate OAuth keys.');
+    }
+  }
+
+}
