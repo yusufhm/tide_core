fb11af7feefbc6ad303e842e3fdf4f4ec492aa06
edyuenyw <edward.yuen@dpc.vic.gov.au>
2022-02-22 09:48:02 +1100

    Adds unit test.

---
+++ tests/src/Unit/TideCommonServicesTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\Tests\tide_core\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\tide_core\TideCommonServices;
+
+/**
+ * Main test class for common functions.
+ */
+class TideCommonServicesTest extends UnitTestCase {
+
+  /**
+   * @covers ::sanitiseFileName
+   * @dataProvider fileNameProvider
+   */
+  public function testSanitiseFileName($value, $replacement, $include_in_pattern, $expected) {
+    $tideCommonServices = new TideCommonServices();
+    $filename = $tideCommonServices->sanitiseFileName($value, $replacement, $include_in_pattern);
+    $this->assertEquals($expected, $filename);
+  }
+
+  /**
+  * Data provider of test sanitise.
+  *
+  * @return array
+  *   Array of values.
+  */
+  public function fileNameProvider() {
+    return [
+      [
+        'value' => 'file with spaces only.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => FALSE,
+        'expected' => 'file-with-spaces-only.pdf',
+      ],
+      [
+        'value' => 'file with spaces only.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => TRUE,
+        'expected' => 'file-with-spaces-only.pdf',
+      ],
+      [
+        'value' => 'file - with - spaces - only.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => FALSE,
+        'expected' => 'file---with---spaces---only.pdf',
+      ],
+      [
+        'value' => 'file - with - spaces - only.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => TRUE,
+        'expected' => 'file-with-spaces-only.pdf',
+      ],
+      [
+        'value' => 'file - with- before- spaces and -after -space.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => TRUE,
+        'expected' => 'file-with-before-spaces-and-after-space.pdf',
+      ],
+      [
+        'value' => 'file - with + spaces _ other ~ symbols.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => FALSE,
+        'expected' => 'file---with-+-spaces-_-other-~-symbols.pdf',
+      ],
+      [
+        'value' => 'file - with + spaces _ other ~ symbols.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => TRUE,
+        'expected' => 'file-with-+-spaces-_-other-~-symbols.pdf',
+      ],
+      [
+        'value' => 'site/2020-02/files/file - with - spaces - only.pdf',
+        'replacement' => '-',
+        'include_in_pattern' => FALSE,
+        'expected' => 'site/2020-02/files/file-with-spaces-only.pdf',
+      ],
+    ];
+  }
+
+}
