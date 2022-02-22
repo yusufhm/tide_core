c5605cc0374ae336b3d14c2971a43298ed696933
Mayur Gondhkar <mayurngondhkar@gmail.com>
2022-02-23 10:05:09 +1100

    Add temp commit that will be reverted later

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=10c0c99d9d1ec20b01d1ef30031cbec078055761
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
