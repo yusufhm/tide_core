0d91ac16d0ad2a44c4913ae6564e42f0f9b30842
MdNadimHossain <getovic@gmail.com>
2022-02-24 10:34:21 +1100

    Added settings file for tide_spell_checker module.

---
+++ modules/tide_spell_checker/tide_spell_checker.settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * tide_spell_checker CKEditor configuration file.
+ */
+
+// Create ckeditor custom config array.
+$ckeditor_config = '';
+$array_of_config = [];
+$envVars = getenv();
+foreach ($envVars as $var => $value) {
+  if (str_contains($var, 'CKEDITOR_')) {
+    $var = strtolower(trim(str_replace('CKEDITOR_','',$var)));
+    $var = ($var == 'scayt_slang') ? 'scayt_sLang' : $var;
+    $array_of_config[$var] = $value;
+  }
+}
+if (isset($array_of_config)) {
+  foreach ($array_of_config as $key => $value) {
+    $ckeditor_config = $ckeditor_config . "\r\n$key = \"$value\"";
+  }
+}
+// Add ckeditor custom config.
+if ($ckeditor_config) {
+  $config['editor.editor.rich_text']['settings']['plugins']['customconfig']['ckeditor_custom_config'] = $ckeditor_config;
+}
